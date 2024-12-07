a68c727a6d4db0e47cd786ed966b8d06ddbb8e5f
Evgenia <118038480+Rey1147@users.noreply.github.com>
2024-12-07 20:15:17 +0300

    добавлена обработка ошибок в первом задании

--- scripts/task1.js
+++ scripts/task1.js
@@ -1,29 +1,34 @@
 export const task1 = () => {    
     const content = document.getElementById('content')
     const button = document.getElementById('getSalary')
+    const error = `<p class="error">Введите корректные данные</p>`
 
-    document.getElementById('salaryInput1').addEventListener('input', () => {
+    const listInputs = ['salaryInput1', 'salaryInput2', 'salaryInput3']
+
+    const handleInput = () => {
         document.querySelector('.task1Result')?.remove()
+        document.querySelector('.error')?.remove()
         button.classList?.remove('button-none')
-    })
-    document.getElementById('salaryInput2').addEventListener('input', () => {
-        document.querySelector('.task1Result')?.remove()
-        button.classList?.remove('button-none')
-    })
-    document.getElementById('salaryInput3').addEventListener('input', () => {
-        document.querySelector('.task1Result')?.remove()
-        button.classList?.remove('button-none')
+    }
+
+    listInputs.forEach(input => {
+        document.getElementById(input).addEventListener('input', handleInput)
     })
 
     button.addEventListener('click', () => {
-        const month1 = parseFloat(document.getElementById("salaryInput1").value) || 0
-        const month2 = parseFloat(document.getElementById("salaryInput2").value) || 0
-        const month3 = parseFloat(document.getElementById("salaryInput3").value) || 0
+        const month1 = parseFloat(document.getElementById("salaryInput1").value)
+        const month2 = parseFloat(document.getElementById("salaryInput2").value)
+        const month3 = parseFloat(document.getElementById("salaryInput3").value)
 
         const sum = (month1 + month2 + month3).toFixed(2)
         const persent = (sum * 0.13).toFixed(2)
         const withoutPersent = (sum * 0.87).toFixed(2)
 
+        if (isNaN(sum)) {
+            button.className = 'button-none'
+            content.insertAdjacentHTML("beforeend", error)
+            return
+        }
 
         const result = `<div class="task1Result">
                         <p>Общая сумма зарплаты: <b/>${sum}</b></p>
